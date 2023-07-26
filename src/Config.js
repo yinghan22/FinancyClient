a7bfceb7ff543567d44a4fd198cc0ec3f636e9b0
嬴寒 <71254143+yinghan22@users.noreply.github.com>
2023-07-26 11:17:03 +0800

    update

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 export default {
     // base_url: 'http://192.168.211.138:80/financy/api',
-    base_url: 'http://127.0.0.1:8000/api',
-    static_url: 'http://192.168.211.138:80/financy',
+    base_url: 'http://127.0.0.1:9000/api',
+    static_url: 'http://127.0.0.1/financy',
 };
